56d15fac913c6c9cfe90d52ffa8c4edf3ca20d75
Ernest Saidu Kamara <ernest.saidu.kamara@dnb.no>
2020-03-28 18:31:47 +0100

    Changed Typography and added links to bio text

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,17 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Ernest Saidu Kamara 🇸🇱🧑🏿‍💻🇳🇴" 
+    const siteTitle = "Ernest Saidu Kamara" 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
+          <p>
+          <h4>Android | Flutter | Scrum Master certified | IoT enthusiast | 🇸🇱🇳🇴👪🏿
+          </h4>        
+          </p>
             <p>
                 Ernest is an Android professional, with experience in developing and maintaining high quality scalable Android applications. His journey 
                 in mobile application development started nine years ago, just after the birth of Apple's App Store.
@@ -22,11 +26,12 @@ class IndexPage extends React.Component {
               <p>
               Following his passion for technology, mainly mobile was one of the best decisions he ever made. He deems himself attractive to the problem-solving aspect of technology and appreciates challenge seeking opportunities to learn and improve his skills. 
             </p>
-            <p>
-                Ernest is currently working as a Senior Android consultant at DNB(MinBank) via Shortcut, previously EVRY (Digital banking services) and Zedge Inc.
+            <p>              
+                Ernest is currently working as a Senior Android consultant at <a href= "https://www.dnb.no">DNB(MinBank)</a> via <a href="https://www.shortcut.no">Shortcut - We Make Apps</a>, previously <a href="https://www.evry.com">EVRY digital banking services</a> and <a href="https://www.zedge.net">ZEDGE</a>.
             </p>
+        
             <p>
-            In his spare time, Ernest enjoys taking photos and playing soccer with his kids when he is not co-organising meetups for his local developer community.
+            In his spare time, Ernest enjoys taking <a href="https://www.instagram.com/instakamara">photos</a> and playing soccer with his kids when he is not co-organising meetups for his local developer community.
             </p>
         <h1>
           I blog too! {" "}
